ee06658084b2de45542f3b49542e08f6a2c85558
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-12-06 10:57:15 -0300

    Created value object and unit tests

--- NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -12,9 +12,10 @@ namespace NerdStore.Catalogo.Domain
         public DateTime DataCadastro { get; private set; }
         public string Imagem { get; private set; }
         public int QuantidadeEstoque { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
         public Categoria Categoria { get; private set; }
 
-        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
         {
             CategoriaId = categoriaId;
             Nome = nome;
@@ -23,6 +24,7 @@ namespace NerdStore.Catalogo.Domain
             Valor = valor;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+            Dimensoes = dimensoes;
 
             Validar();
         }
