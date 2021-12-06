dcd43e83110f9bbdcd1fc5e684f85798d4d087b3
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-12-06 15:15:14 -0300

    Added Repository

---
+++ NerdStore/src/NerdStore.Catalogo.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,62 @@
+﻿using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+
+namespace NerdStore.Catalogo.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _context;
+
+        public ProdutoRepository(CatalogoContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork =>_context;
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _context.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _context.Produtos.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterPorCategoria(int codigo)
+        {
+            return await _context.Produtos.AsNoTracking().Include(p => p.Categoria).Where(c => c.Categoria.Codigo == codigo).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Categoria>> ObterCategorias()
+        {
+            return await _context.Categorias.AsNoTracking().ToListAsync();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+        }
+
+        public void Adicionar(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+        }
+
+        public void Atualizar(Categoria categoria)
+        {
+            _context.Categorias.Update(categoria);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
