e62cda8fd90ec85d1076fe1a08c607252a3ca31f
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-12-08 11:45:01 -0300

    Added EstoqueService as a domain service

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,45 @@
+﻿namespace NerdStore.Catalogo.Domain
+{
+    /// <summary>
+    /// EstoqueService é um serviço de domínio
+    /// </summary>
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+            produto.ReporEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository.Dispose();
+        }
+    }
+}
---
+++ NerdStore/src/NerdStore.Catalogo.Domain/IEstoqueService.cs
@@ -0,0 +1,8 @@
+﻿namespace NerdStore.Catalogo.Domain
+{
+    public interface IEstoqueService : IDisposable
+    {
+        Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+        Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+    }
+}
