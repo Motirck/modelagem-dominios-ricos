a93b6c628e6d9a2a25ba9ed256ec174b8fc3bb53
Ricardo Alves Paula <rapaula@jacksondawson.com>
2021-12-26 11:59:04 -0300

    Added new class library (Application) and created the Dtos, Service for Product and mappings

---
+++ NerdStore/src/NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,18 @@
+﻿using NerdStore.Catalogo.Application.Dtos;
+
+namespace NerdStore.Catalogo.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoDto>> ObterPorCategoria(int codigo);
+        Task<ProdutoDto> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoDto>> ObterTodos();
+        Task<IEnumerable<CategoriaDto>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoDto produtoViewModel);
+        Task AtualizarProduto(ProdutoDto produtoViewModel);
+
+        Task<ProdutoDto> DebitarEstoque(Guid id, int quantidade);
+        Task<ProdutoDto> ReporEstoque(Guid id, int quantidade);
+    }
+}
