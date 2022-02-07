48e1c3e9ad957910faa5abe6a2c6d7c71830f567
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2022-02-07 19:11:54 -0300

    Dropped database and generated migrations again

--- NerdStore/src/NerdStore.Catalogo.Data/Migrations/20220207221050_Initial.Designer.cs
+++ NerdStore/src/NerdStore.Catalogo.Data/Migrations/20220207221050_Initial.Designer.cs
@@ -12,7 +12,7 @@ using NerdStore.Catalogo.Data;
 namespace NerdStore.Catalogo.Data.Migrations
 {
     [DbContext(typeof(CatalogoContext))]
-    [Migration("20220207220503_Initial")]
+    [Migration("20220207221050_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
