fac6df5cfc87c41e19e5db88ddc71499c57e0d07
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2022-03-05 10:50:40 -0300

    Implemented Anti-Corruption layer

---
+++ NerdStore/src/NerdStore.Pagamentis.AntiCorruption/IPayPalGateway.cs
@@ -0,0 +1,8 @@
+﻿namespace NerdStore.Pagamentos.AntiCorruption;
+
+public interface IPayPalGateway
+{
+    string GetPayPalServiceKey(string apiKey, string encriptionKey);
+    string GetCardHashKey(string serviceKey, string cartaoCredito);
+    bool CommitTransaction(string cardHashKey, string orderId, decimal amount);
+}
---
+++ NerdStore/src/NerdStore.Pagamentis.AntiCorruption/PayPalGateway.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace NerdStore.Pagamentos.AntiCorruption;
+
+public class PayPalGateway : IPayPalGateway
+{
+    public bool CommitTransaction(string cardHashKey, string orderId, decimal amount)
+    {
+        return new Random().Next(2) == 0; // é como se fosse uma roleta russa pois as vezes será aprovado e as vezes nao
+        //return false;
+    }
+
+    public string GetCardHashKey(string serviceKey, string cartaoCredito)
+    {
+        return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10)
+            .Select(s => s[new Random().Next(s.Length)]).ToArray());
+    }
+
+    public string GetPayPalServiceKey(string apiKey, string encriptionKey)
+    {
+        return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10)
+            .Select(s => s[new Random().Next(s.Length)]).ToArray());
+    }
+}
